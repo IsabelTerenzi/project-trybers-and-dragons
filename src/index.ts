2d71f23c793a89c5a31715592c03b66e75d065ed
Isabel Terenzi <bel.terenzi@gmail.com>
2022-10-19 22:08:40 -0300

    reenviando requisito 10

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Battle, { PVE, PVP } from './Battle';
+import Battle, { PVP } from './Battle';
 import Character from './Character';
 import Dragon from './Dragon';
 import Monster from './Monster';
@@ -17,7 +17,7 @@ const monster2 = new Dragon();
 
 const pvp = new PVP(player2, player3);
 
-const pve = new PVE(player1, [monster1, monster2]);
+// const pve = new PVE(player1, [monster1, monster2]);
 
 function runBattles(battles: Battle[]) {
   battles.forEach((battle) => battle.fight());
